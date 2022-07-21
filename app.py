6fca8a84115b7ea623dda913a8b6533f2207cfcd
krapkaVKomi <98738329+krapkaVKomi@users.noreply.github.com>
2022-07-21 19:04:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import email
+from unicodedata import name
 from flask import Flask, request, redirect, render_template, url_for, redirect
+from clear import clear
 import sqlite3
 import datetime
+import random
+
+## Iдеї для оновлень
+# особистий кабінет автора + модерація статтей
+# Рейтинг статей і авторів (фунція лайку + чарт статтей)
 
 
 app = Flask(__name__)
@@ -8,12 +16,26 @@ database = sqlite3.connect('blog2.db')
 cursor = database.cursor()
 database.execute('CREATE TABLE IF NOT EXISTS {}(id, title, intro, text, date, user)' .format('data2'))
 database.commit()
-
+database.execute('CREATE TABLE IF NOT EXISTS {}(id, quote, author)' .format('data_quotes'))
+database.commit()
+database.execute('CREATE TABLE IF NOT EXISTS {}(id, name, email, password, date_registration, author_rating)' .format('users'))
+database.commit()
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    database = sqlite3.connect('blog2.db')
+    cursor = database.cursor()
+    qoute_id = cursor.execute('SELECT id FROM data_quotes').fetchall()
+    random_id = random.randrange(int(clear(qoute_id[0])), int(clear(qoute_id[-1])))
+    random_id = str(random_id)
+    quote = cursor.execute('SELECT quote FROM data_quotes WHERE id == ?', (random_id,)).fetchone()
+    quote = clear(quote)
+    author = cursor.execute('SELECT author FROM data_quotes WHERE id == ?', (random_id,)).fetchone()
+    author = clear(author)
+    quotes = [quote, author]
+    return render_template('index.html', quotes=quotes)
+
 
 
 @app.route('/about')
@@ -21,10 +43,21 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post')
-def post():
-    
-    return render_template('post.html')
+
+
+@app.route('/user')
+def user():
+    database = sqlite3.connect('blog2.db')
+    cursor = database.cursor()
+    qoute_id = cursor.execute('SELECT id FROM data_quotes').fetchall()
+    random_id = random.randrange(int(clear(qoute_id[0])), int(clear(qoute_id[-1])))
+    random_id = str(random_id)
+    quote = cursor.execute('SELECT quote FROM data_quotes WHERE id == ?', (random_id,)).fetchone()
+    quote = clear(quote)
+    author = cursor.execute('SELECT author FROM data_quotes WHERE id == ?', (random_id,)).fetchone()
+    author = clear(author)
+    quotes = [quote, author]
+    return render_template('user.html', quotes=quotes)
 
 
 @app.route('/posts')
@@ -34,15 +67,26 @@ def posts():
     art_title = cursor.execute('SELECT title FROM data2').fetchall()
     art_intro = cursor.execute('SELECT intro FROM data2').fetchall()
     art_time = cursor.execute('SELECT date FROM data2').fetchall()
+    art_id = cursor.execute('SELECT id FROM data2').fetchall()
     article = []
     for i in range(len(art_title)-1, -1, -1):
         box = []
         box.append(art_title[i])
         box.append(art_intro[i])
         box.append(art_time[i])
+        id = str(art_id[i])
+        page_id = ''
+        for i in id:
+            if i != '(':
+                if i != ')':
+                    if i != ',':
+                        if i != "'":
+                            if i != '"':
+                                page_id += i
+        box.append(page_id)
         article.append(box)
-    return render_template('posts.html', article=article)
-  
+    return render_template('posts.html', article=article)   
+
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
@@ -61,9 +105,9 @@ def create_article():
                             if i != "'":
                                 if i != '"':
                                     new_id += i 
-            id = int(new_id)+1        
+            id = str(int(new_id)+1)       
         else:
-            id = 0
+            id = '0'
         
         
         user = 'test_name'
@@ -78,19 +122,58 @@ def create_article():
         date = ''
         for i in time:
             date += i
+
+        artitle = [title, intro, time, text]
        
         cursor.execute('INSERT INTO data2 VALUES(?, ?, ?, ?, ?, ?)', (id, title, intro, text, date, user))
         database.commit()
-        return render_template('post.html')
+        return render_template('post.html', artitle=artitle)
         
     else:
         return render_template('create-article.html')
 
-'''
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page: " + name + "-" + str(id)
-'''
+
+@app.route('/posts/<string:id>')
+def post(id):
+    database = sqlite3.connect('blog2.db')
+    cursor = database.cursor()
+    art_title = cursor.execute('SELECT title FROM data2 WHERE id == ?', (id,)).fetchone()
+    art_title = clear(art_title)
+    art_intro = cursor.execute('SELECT intro FROM data2 WHERE id == ?', (id,)).fetchone()
+    art_intro = clear(art_intro)
+    art_time = cursor.execute('SELECT date FROM data2 WHERE id == ?', (id,)).fetchone()
+    art_time = clear(art_time)
+    art_text = cursor.execute('SELECT text FROM data2 WHERE id == ?', (id,)).fetchone()
+    art_text = clear(art_text)
+    artitle = [art_title, art_intro, art_time, art_text]
+    return render_template('post.html', artitle=artitle)   
+
+
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    if request.method == "POST":
+        database = sqlite3.connect('blog2.db')
+        cursor = database.cursor()
+    
+        id = 'test_id'
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        date_registration = 'test_date_registration'
+        author_rating = 'test_author_rating'
+        user = [name, email, password]
+        cursor.execute('INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)', (id, name, email, password, date_registration, author_rating))
+        database.commit()
+   
+        return render_template('registration.html', user=user)
+
+    else:
+        return render_template('registration.html')
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=False)
+
